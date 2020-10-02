22ca4898370e5b0b7d1984faea7236d13a21a65e
Idjaz Hossanee <idjazhossanee@gmail.com>
2020-10-02 23:02:57 +0400

    templating in progress

--- notes_project/notes/views.py
+++ notes_project/notes/views.py
@@ -1,15 +1,19 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 
 
+@login_required
 def list(request):
     return render(request, 'notes/list.html')
 
 
+@login_required
 def create(request):
     return render(request, 'notes/list.html')
 
 
+@login_required
 def update(request, note_id):
     return render(request, 'notes/list.html')
