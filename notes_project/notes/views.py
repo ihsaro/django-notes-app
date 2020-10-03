91c5123789b3753bf5787be74c1f51d26647927f
Idjaz Hossanee <idjazhossanee@gmail.com>
2020-10-03 22:48:21 +0400

    crud in progress

--- notes_project/notes/views.py
+++ notes_project/notes/views.py
@@ -15,16 +15,42 @@ def list_view(request):
 
 @login_required
 def create_view(request):
-    return render(request, 'notes/create.html')
+    if request.method == 'POST':
+        note_title = request.POST['note-title']
+        note_type = request.POST['note-type']
+        note_description = request.POST['note-description']
+
+        note = Note(note_title=note_title, note_type=note_type,
+                    note_description=note_description, user=request.user)
+        note.save()
+        if note.id is not None:
+            return redirect('notes:list')
+        else:
+            return redirect('notes:create')
+    else:
+        return render(request, 'notes/create.html')
 
 
 @login_required
 def update_view(request, note_id):
-    current_user = request.user
-    try:
-        context = {
-            'note': Note.objects.all().filter(user=current_user).get(pk=note_id)
-        }
-        return render(request, 'notes/create.html', context)
-    except Note.DoesNotExist:
+    if request.method == 'POST':
+        note_title = request.POST['note-title']
+        note_type = request.POST['note-type']
+        note_description = request.POST['note-description']
+
+        note = Note.objects.get(pk=note_id)
+        note.note_title = note_title
+        note.note_type = note_type
+        note.note_description = note_description
+        note.save()
+
         return redirect('notes:list')
+    else:
+        current_user = request.user
+        try:
+            context = {
+                'note': Note.objects.all().filter(user=current_user).get(pk=note_id)
+            }
+            return render(request, 'notes/create.html', context)
+        except Note.DoesNotExist:
+            return redirect('notes:list')
