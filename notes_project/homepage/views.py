4d404d6b929556b04d3289f28861dec3f9e53b70
Idjaz Hossanee <idjazhossanee@gmail.com>
2020-10-03 20:44:33 +0400

    crud in progress

--- notes_project/homepage/views.py
+++ notes_project/homepage/views.py
@@ -1,7 +1,9 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
 # Create your views here.
 
 
 def index_view(request):
-    return render(request, 'homepage/index.html')
+    if request.user.is_authenticated:
+        return redirect('notes:list')
+    else:
+        return render(request, 'homepage/index.html')
--- notes_project/notes/views.py
+++ notes_project/notes/views.py
@@ -1,19 +1,30 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+from .models import Note
 
 
 @login_required
 def list_view(request):
-    return render(request, 'notes/list.html')
+    current_user = request.user
+    context = {
+        'notes': Note.objects.all().filter(user=current_user)
+    }
+    return render(request, 'notes/list.html', context)
 
 
 @login_required
 def create_view(request):
-    return render(request, 'notes/list.html')
+    return render(request, 'notes/create.html')
 
 
 @login_required
 def update_view(request, note_id):
-    return render(request, 'notes/list.html')
+    current_user = request.user
+    try:
+        context = {
+            'note': Note.objects.all().filter(user=current_user).get(pk=note_id)
+        }
+        return render(request, 'notes/create.html', context)
+    except Note.DoesNotExist:
+        return redirect('notes:list')
