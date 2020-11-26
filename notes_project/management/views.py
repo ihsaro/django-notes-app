0c3c4b5e2c3867e35f14b04b4c66ca3e560ff5a1
Idjaz Hossanee <idjazhossanee@gmail.com>
2020-11-26 23:41:35 +0400

    list customers in progress

--- notes_project/management/views.py
+++ notes_project/management/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
 from authentication.decorators import allowed_groups
+from notes.models import Note
 
 
 @login_required
@@ -15,8 +16,16 @@ def list_customers_view(request):
         for group in request.user.groups.all():
             groups_names.append(group.name)
 
+        customers = []
+
+        for user in User.objects.all():
+            customers.append({
+                'user_details': user,
+                'number_of_notes': Note.objects.filter(user=user).count()
+            })
+
         context = {
-            'customers': User.objects.filter(groups__name='customer'),
+            'customers': customers,
             'groups_names': groups_names
         }
         return render(request, 'management/list_customers.html', context=context)
